4f85256a55366659bbfe31e0d873f154130ebaef
Paulino Fonseca <paulinofonseca@gmail.com>
2023-10-09 18:33:31 +0100

    ajuste de fonte nas entradas e saidas

--- lib/app/modules/home/components/entradas_saidas.dart
+++ lib/app/modules/home/components/entradas_saidas.dart
@@ -4,6 +4,7 @@ import 'package:app_financas/app/modules/home/controllers/home_page_controller.d
 import 'package:app_financas/constants.dart';
 import 'package:app_financas/helders/format_helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gutter/flutter_gutter.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -84,33 +85,40 @@ class EntradaOuSaidaWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.center,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SvgPicture.asset(asset),
-        SizedBox(width: kDefaultPadding),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              title,
-              style: GoogleFonts.inter(
-                fontSize: 14,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              numberFormat.format(valor),
-              style: GoogleFonts.inter(
-                fontSize: 22,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
+            SvgPicture.asset(asset, width: 16),
+            SizedBox(width: kDefaultPadding / 4),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: GoogleFonts.inter(
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
           ],
+        ),
+        GutterTiny(),
+        Text(
+          numberFormat.format(valor),
+          style: GoogleFonts.inter(
+            fontSize: 16,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     );
