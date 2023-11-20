fa3cf21232555f5269cac63f2879e1909525ecb6
Paulino Fonseca <paulinofonseca@gmail.com>
2023-11-20 19:10:43 +0100

    feat: adiciona a categoria bloc

---
+++ lib/presentation/bloc/categoria/categoria_bloc.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:app_financas/core/domain/entitys/categoria_movimento.dart';
+import 'package:app_financas/core/domain/services/i_categoria_service.dart';
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+part 'categoria_event.dart';
+part 'categoria_state.dart';
+
+class CategoriaBloc extends Bloc<CategoriaEvent, CategoriaState> {
+  late final ICategoriaService _categoriaService;
+
+  CategoriaBloc(this._categoriaService) : super(CategoriaInitial()) {
+    on<GetCategoriaByIdEvent>(_onCategoriaCategoriaByIdEvent);
+  }
+
+  void _onCategoriaCategoriaByIdEvent(GetCategoriaByIdEvent event, emit) {
+    emit(CategoriaLoading());
+
+    if (event.categoriaTipoId == 1) {
+      _categoriaService.getEntradaCategoria(event.id).then((value) {
+        if (value is Right) {
+          emit(CategoriaSuccess(value.getOrElse(() => Categoria.fake())));
+        } else {
+          emit(const CategoriaError('Erro ao buscar categoria'));
+        }
+      });
+    } else {
+      _categoriaService.getSaidaCategoria(event.id).then((value) {
+        if (value is Right) {
+          emit(CategoriaSuccess(value.getOrElse(() => Categoria.fake())));
+        } else {
+          emit(const CategoriaError('Erro ao buscar categoria'));
+        }
+      });
+    }
+  }
+}
