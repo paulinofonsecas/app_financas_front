a980cafea1505451af61911cfadc4167967a8420
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-08-28 20:07:21 +0100

    ajustes finos

--- lib/presentation/modules/estatisticas/estatisticas_page.dart
+++ lib/presentation/modules/estatisticas/estatisticas_page.dart
@@ -1,15 +1,14 @@
 import 'package:app_financas/constants.dart';
-import 'package:app_financas/core/domain/entitys/tipo_movimento.dart';
 import 'package:app_financas/presentation/components/periodo_picker_widget.dart';
+import 'package:app_financas/presentation/modules/estatisticas/components/tipo_movimento_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gutter/flutter_gutter.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../carteira/components/my_text_filter.dart';
-import 'graficos/estatistica_por_categoria.dart';
 import 'controller/estatisticas_page_controller.dart';
 import 'graficos/estatistica_line_da_semana.dart';
+import 'graficos/estatistica_por_categoria.dart';
 
 class EstatisticasPage extends StatefulWidget {
   const EstatisticasPage({super.key});
@@ -71,6 +70,8 @@ class _EstatisticasPageState extends State<EstatisticasPage> {
           ),
         ),
         const Gutter(),
+        const TipoMovimentosWidget(),
+        const Gutter(),
         Obx(
           () => PeriodoPickerWidget(
             periodoMes: controller.mygetMonthName(controller.periodoMes.value),
@@ -96,23 +97,6 @@ class _EstatisticasPageState extends State<EstatisticasPage> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           const GutterTiny(),
-          MyTextFilter(
-            title: 'Saídas',
-            isActive: controller.esFilter == TipoMovimento.SAIDA,
-            onTap: () {
-              controller.changeESFilter(TipoMovimento.SAIDA);
-            },
-          ),
-          const GutterTiny(),
-          MyTextFilter(
-            title: 'Entrada',
-            isActive: controller.esFilter == TipoMovimento.ENTRADA,
-            onTap: () {
-              controller.changeESFilter(TipoMovimento.ENTRADA);
-            },
-          ),
-          const Spacer(),
-          // select periodo
           DropdownButton<int>(
             value: controller.periodoId,
             onChanged: (int? value) {
